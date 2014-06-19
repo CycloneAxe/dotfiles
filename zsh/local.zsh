b2bb0dbfe7551c9fa518529bdf050c39d5ed2c0f
CycloneAxe <matrixlity@gmail.com>
2014-06-19 10:32:32 +0800

    修改zsh配置

--- zsh/local.zsh
+++ zsh/local.zsh
@@ -1,6 +1,23 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 # plugins=(git svn autojump)
 
+#path=(
+#	/usr/local/bin
+#	/usr/bin
+#	/bin
+#	/usr/sbin
+#	/sbin
+#	/usr/local/bin
+#)
+
+DEV_DIR="$HOME/Workspace"
+WWW_DIR="$HOME/Sites"
+
+hash -d dev="$DEV_DIR"
+hash -d www="$WWW_DIR"
+hash -d laravel="$DEV_DIR/laravel"
+hash -d github="$DEV_DIR/github"
+
 function tree() {
 	_args="$@"
 	_path=${_args:-.}
